d5be686336fb23487ca293bbfb94d1ece87796eb
zhufeng <zhufeng@youzan.com>
2019-08-30 10:57:20 +0800

    temp

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,40 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
+import { terser } from "rollup-plugin-terser";
 
-import pkg from './package.json'
+import pkg from "./package.json";
+
+// node rollup -c -w
+const isBuild = process.argv.length < 4 ? true : false;
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
+      format: "cjs",
+      sourcemap: !isBuild
     },
     {
       file: pkg.module,
-      format: 'es',
-      sourcemap: true
+      format: "es",
+      sourcemap: !isBuild
     }
   ],
   plugins: [
     external(),
-    postcss({
-      modules: true
-    }),
+    postcss(),
     url(),
     babel({
-      exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      exclude: "node_modules/**",
+      plugins: ["external-helpers"]
     }),
     resolve(),
-    commonjs()
+    commonjs(),
+    (isBuild && terser())
   ]
-}
+};
